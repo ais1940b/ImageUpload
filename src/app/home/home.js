dc0f3371e403ad6d6f22a49d0df2d1a5c64e240b
ais1940b <ais1940b@users.noreply.github.com>
2017-07-27 16:43:00 +0530

    Update home.js

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -51,7 +51,7 @@ angular.module('ngBoilerplate.home', [
             //Append the file to the form data.
             var request = new XMLHttpRequest();
             var requestWebServiceUrl = '';8
-            requestWebServiceUrl = 'https://api2.directscaledev.com/api/Customers/SetCustomerImage';
+            requestWebServiceUrl = 'API URL';
             request.open('POST', requestWebServiceUrl, true);
             request.setRequestHeader('Accept', 'application/json');
             var token = 'LbmQSB1lC-vg5B679wTeqOEKgw4OXf_9mKGup84bLguqlL8yhktvGkbsnETOuzC3sJSLRvFdXxZh_-nNIVkW7TjekkZyUmceHY3xTwZ-7vjUOVNRih77F5htw_ALapQicNhsjbffTPXJWuE7mHTN-W6NnWPQxzXH5S773MNOYLCjuIe9XHzyIhZgtmkvZE8L8UVVaJIespTE_wsoRGskEMKGZlg6wD5zvEtOvqy1sucEwPHulEnOOEx2ROXx8CrRVi2gqUGpqgZw4b4t3G5H_bbab0KcayV6zO9RHv_hVxzEkYntgmYxBhPAAt-EkB8MsrflIT2lFzpOgYCp770K7ZR9z8b7Pnf1P40VuJehY9OGbe5AI7uJ_9xRGAtsI_kYYSp_xkVT3EY9o4MZEPmCPOL9RYTeN-vO8S8gmD6WPIR65hTMk6d0QAFeI8_AjFM9Kc6GPT3wxo7HjLHg2-esF0n9yJC3WzBIkpISQfFaL_fvLvBzYgIDjOO6zERTwnu34Nml70zEmJbWtSwyjbXBPw9oEdrYSr3uGQar6ka4bn4XJHLzdRA751-IXHz3qT4wtjsCTyL6X2H96vqehX2egdq6np6XnFkleAXRA9UQgqEjeQ5c';
@@ -61,9 +61,9 @@ angular.module('ngBoilerplate.home', [
             request.send(profileImage);
             request.onreadystatechange = function () {
                 if (request.readyState == 4) {
-                    //if (request.status == 200) {
+                    if (request.status == 200) {
                         alert('Image Uploaded Successfully');
-                   // }
+                    }
                 }
             };
         } catch (ex) {
